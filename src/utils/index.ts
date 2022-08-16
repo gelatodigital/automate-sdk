8515b19c1e015910e16cd7a550ce8a7928d9692c
Brandon Chuah <songlin.chuah@gmail.com>
2022-08-16 12:51:45 +0200

    feat: support ops module

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,5 @@ import { GELATO_ADDRESSES } from "../constants";
 export function isAutomateSupported(chainId: number): boolean {
   return Boolean(GELATO_ADDRESSES[chainId]);
 }
+
+export * from "./taskModules";
