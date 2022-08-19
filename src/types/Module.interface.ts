3b659981eeb39d2d8fae24972fb91a30d28cf9f8
brandon.cs <brandonchuah.sl@gmail.com>
2022-08-19 18:19:00 +0200

    feat: ops module class (#18)
    * feat: GelatoOpsModule helper class
    
    * fix: missing export

--- src/types/Module.interface.ts
+++ src/types/Module.interface.ts
@@ -12,3 +12,31 @@ export interface ModuleData {
   modules: Module[];
   args: string[];
 }
+
+export interface ModuleArgs
+  extends Partial<ResolverParams>,
+    Partial<TimeParams>,
+    Partial<ProxyParams>,
+    Partial<SingleExecParams>,
+    Partial<OffChainResolverParams> {}
+
+export interface ResolverParams {
+  resolverAddress: string | null;
+  resolverData: string | null;
+}
+
+export interface TimeParams {
+  startTime: number | null;
+  interval: number | null;
+}
+
+export interface ProxyParams {
+  proxy: boolean | null;
+}
+export interface SingleExecParams {
+  singleExec: boolean | null;
+}
+export interface OffChainResolverParams {
+  offChainResolverHash: string | null;
+  offChainResolverArgs: { [key: string]: unknown } | null;
+}
