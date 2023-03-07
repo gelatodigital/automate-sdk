dfbb7ed33733cb576e3113a92ab5333f15115f94
brandon.cs <songlin.chuah@gmail.com>
2023-03-08 01:53:25 +0800

    feat: arb goerli (#37)
    * feat: arbitrum goerli support
    
    * chore: bump version

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -36,7 +36,7 @@ export const GELATO_ADDRESSES: GelatoAddressBook = {
     ops: "",
   },
   [CHAIN_ID.ARBITRUM_GOERLI]: {
-    ops: "",
+    ops: "0x2A6C106ae13B558BB9E2Ec64Bd2f1f7BEFF3A5E0",
   },
   [CHAIN_ID.MATIC]: {
     ops: "0x2A6C106ae13B558BB9E2Ec64Bd2f1f7BEFF3A5E0",
