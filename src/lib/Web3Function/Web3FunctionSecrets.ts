eff47f6324d2544fbaee19c11629910ce2d1882f
Brandon Chuah <songlin.chuah@gmail.com>
2023-03-27 14:16:02 +0800

    Revert "chore: taskId specific secrets"
    This reverts commit fec6b9a4d5ef479ef7f11e2241ea5643194cebe5.

--- src/lib/Web3Function/Web3FunctionSecrets.ts
+++ src/lib/Web3Function/Web3FunctionSecrets.ts
@@ -18,17 +18,14 @@ export class Web3FunctionSecrets {
     this._signature = signature;
   }
 
-  public async get(taskId: string, key: string): Promise<string> {
+  public async get(key: string): Promise<string> {
     try {
       const address = await this._signer.getAddress();
       const authToken = await this._signature.getAuthToken();
 
-      const res = await this._userApi.get(
-        `/users/${address}/${taskId}/secrets/${key}`,
-        {
-          headers: { Authorization: `Bearer ${authToken}` },
-        }
-      );
+      const res = await this._userApi.get(`/users/${address}/secrets/${key}`, {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
 
       const secret = res.data[key];
 
@@ -39,17 +36,14 @@ export class Web3FunctionSecrets {
     }
   }
 
-  public async list(taskId: string): Promise<Secrets> {
+  public async list(): Promise<Secrets> {
     try {
       const address = await this._signer.getAddress();
       const authToken = await this._signature.getAuthToken();
 
-      const res = await this._userApi.get(
-        `/users/${address}/${taskId}/secrets`,
-        {
-          headers: { Authorization: `Bearer ${authToken}` },
-        }
-      );
+      const res = await this._userApi.get(`/users/${address}/secrets`, {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
 
       return res.data as Secrets;
     } catch (err) {
@@ -58,13 +52,13 @@ export class Web3FunctionSecrets {
     }
   }
 
-  public async set(taskId: string, secrets: Secrets): Promise<void> {
+  public async set(secrets: Secrets): Promise<void> {
     try {
       const address = await this._signer.getAddress();
       const authToken = await this._signature.getAuthToken();
 
       await this._userApi.post(
-        `/users/${address}/${taskId}/secrets`,
+        `/users/${address}/secrets`,
         { ...secrets },
         {
           headers: { Authorization: `Bearer ${authToken}` },
@@ -76,12 +70,12 @@ export class Web3FunctionSecrets {
     }
   }
 
-  public async delete(taskId: string, key: string): Promise<void> {
+  public async delete(key: string): Promise<void> {
     try {
       const address = await this._signer.getAddress();
       const authToken = await this._signature.getAuthToken();
 
-      await this._userApi.delete(`/users/${address}/${taskId}/secrets/${key}`, {
+      await this._userApi.delete(`/users/${address}/secrets/${key}`, {
         headers: { Authorization: `Bearer ${authToken}` },
       });
     } catch (err) {
