01439b7c67c3102babf7fba8086f8e5fed93abf6
Brandon Chuah <songlin.chuah@gmail.com>
2023-04-20 15:12:12 +0800

    chore: add types

--- src/contracts/types/index.ts
+++ src/contracts/types/index.ts
@@ -2,11 +2,13 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { Automate } from "./Automate";
+export type { AutomateProxy } from "./AutomateProxy";
 export type { AutomateProxyFactory } from "./AutomateProxyFactory";
 export type { Forwarder } from "./Forwarder";
 export type { ProxyModule } from "./ProxyModule";
 export * as factories from "./factories";
 export { Automate__factory } from "./factories/Automate__factory";
+export { AutomateProxy__factory } from "./factories/AutomateProxy__factory";
 export { AutomateProxyFactory__factory } from "./factories/AutomateProxyFactory__factory";
 export { Forwarder__factory } from "./factories/Forwarder__factory";
 export { ProxyModule__factory } from "./factories/ProxyModule__factory";
