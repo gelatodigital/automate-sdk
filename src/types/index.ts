98da6483ed8c496df11f7c201f72425d9de7466f
Muhammed Kadir Yücel <kadir@gelato.digital>
2023-08-03 11:45:47 +0300

    chore: export Trigger interface types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,12 @@
+export * from "./ChainId.type";
 export * from "./CreateTaskOptions.interface";
 export * from "./GelatoAddressBook.interface";
+export * from "./Module.interface";
 export * from "./Task.interface";
 export * from "./TaskApiParams.interface";
 export * from "./TaskPopulatedTransaction.interface";
 export * from "./TaskTransaction.interface";
-export * from "./Module.interface";
+export * from "./Trigger.interface";
 export * from "./Web3Function.interface";
 export * from "./Web3FunctionSchema.interface";
-export * from "./ChainId.type";
+
