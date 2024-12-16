c69878870281be59a379afd16b68681dec91e6d4
Denis Panov <denis.panov@gmx.de>
2024-12-16 14:37:37 +0100

    feat: support ink and sonic (#106)
    * feat: added ink and sonic
    
    * chore: bumped version

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -74,6 +74,8 @@ export const CHAIN_ID = {
   INKSEPOLIA: 763373,
   VOLMEX: 123420000588,
   STORY_ODYSSEY_TESTNET: 1516,
+  SONIC: 146,
+  INK: 57073,
 };
 
 export const GELATO_ADDRESSES: GelatoAddressBook = {
@@ -264,6 +266,12 @@ export const GELATO_ADDRESSES: GelatoAddressBook = {
     automate: "0xafd37d0558255aA687167560cd3AaeEa75c2841E",
   },
   [CHAIN_ID.STORY_ODYSSEY_TESTNET]: {
+    automate: "0xafd37d0558255aA687167560cd3AaeEa75c2841E",
+  },
+  [CHAIN_ID.SONIC]: {
+    automate: "0xafd37d0558255aA687167560cd3AaeEa75c2841E",
+  },
+  [CHAIN_ID.INK]: {
     automate: "0xafd37d0558255aA687167560cd3AaeEa75c2841E",
   },
 };
